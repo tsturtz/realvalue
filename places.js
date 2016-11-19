b0a8bbc2bbd06f3239405f0011de2cb3062b789b
Taylor Sturtz <taylorsturtz.com>
2016-11-18 20:48:29 -0800

    working on getting place details

--- places.js
+++ places.js
@@ -5,6 +5,7 @@
 var map;
 var service;
 var infowindow;
+var placeType = 'restaurant'; // Change this var depending on the type of place currently being searched.
 
 /**
  * Firebase config
@@ -25,23 +26,24 @@ var fb = firebase.database();
  * Init google map
  */
 
-function initialize() {
+function initMap() {
     var lfz = new google.maps.LatLng(33.633998,-117.733383);
 
     map = new google.maps.Map(document.getElementById('map'), {
         center: lfz,
-        zoom: 15
+        zoom: 15,
+        styles: [{"featureType":"landscape","stylers":[{"hue":"#FFBB00"},{"saturation":43.400000000000006},{"lightness":37.599999999999994},{"gamma":1}]},{"featureType":"road.highway","stylers":[{"hue":"#FFC200"},{"saturation":-61.8},{"lightness":45.599999999999994},{"gamma":1}]},{"featureType":"road.arterial","stylers":[{"hue":"#FF0300"},{"saturation":-100},{"lightness":51.19999999999999},{"gamma":1}]},{"featureType":"road.local","stylers":[{"hue":"#FF0300"},{"saturation":-100},{"lightness":52},{"gamma":1}]},{"featureType":"water","stylers":[{"hue":"#0078FF"},{"saturation":-13.200000000000003},{"lightness":2.4000000000000057},{"gamma":1}]},{"featureType":"poi","stylers":[{"hue":"#00FF6A"},{"saturation":-1.0989010989011234},{"lightness":11.200000000000017},{"gamma":1}]}]
     });
 
     var request = {
         location: lfz,
-        radius: '1000',
-        types: ['restaurant']
+        radius: '3000',
+        type: [placeType]
     };
 
     infowindow = new google.maps.InfoWindow();
     service = new google.maps.places.PlacesService(map);
-    service.nearbySearch(request, callback);
+    service.radarSearch(request, callback);
 }
 
 /**
@@ -59,22 +61,25 @@ function callback(results, status) {
              * @type {{name: *, address: *}}
              */
 
-            var placeDB = {
-                name: results[i].name,
-                address: results[i].vicinity
+            var placeLoc = {
+                lat: results[i].geometry.location.lat(),
+                lng: results[i].geometry.location.lng()
             };
 
+            console.log(placeType, results[i].place_id, placeLoc);
+
             (function(places) {
-                fb.ref('places/' + (i+1)).set(placeDB);
+                fb.ref(placeType + '/' + results[i].place_id).set(placeLoc);
             })(results[i]);
 
             /**
-             * add places to markers on map
+             * Add markers
              */
 
             var place = results[i];
-            createMarker(place);
+            createMarker(place, placeLoc);
             console.log(results[i]);
+            //initMap2(place);
         }
     }
 }
@@ -82,9 +87,10 @@ function callback(results, status) {
 /**
  * Create markers and set marker content based on the lat/lon 'center' location in map init
  * @param place
+ * @param location
  */
 
-function createMarker(place) {
+function createMarker(place, location) {
     //var placeLoc = place.geometry.location;
     var marker = new google.maps.Marker({
         map: map,
@@ -92,9 +98,30 @@ function createMarker(place) {
     });
 
     google.maps.event.addListener(marker, 'click', function() {
-        infowindow.setContent(place.name + "<br />" + place.vicinity +"<br />" + "rating: " + place.rating);
+        infowindow.setContent('PLACE TYPE: ' + placeType + '<br>' + 'PLACE ID: ' + place.place_id + '<br>' + 'LAT: ' + location.lat + '<br>' + 'LNG: ' + location.lng);
         infowindow.open(map, this);
     });
 }
 
+/*function initMap2() {
+    service = new google.maps.places.PlacesService(map);
+    service.getDetails({
+        placeId: "ChIJl_N4tlno3IARWDJLc0k1zX0" //place.place_id
+    }, function(place) {
+        console.log(
+            'NAME: ' + place.name + '\n',
+            'ADDRESS: ' + place.formatted_address + '\n',
+            'PHONE: ' + place.formatted_phone_number + '\n',
+            'ICON: ' + place.icon + '\n',
+            'RATING: ' + place.rating + '\n',
+            'URL: ' + place.url + '\n',
+            'ZIP CODE: ' + place.address_components[6].short_name + '\n',
+            'LAT: ' + place.geometry.location.lat() + '\n',
+            'LNG: ' + place.geometry.location.lng()
+        );
+    });
+}*/
+
 // note: custom markers - https://developers.google.com/maps/documentation/javascript/custom-markers
+
+// note: add time stamp - {obj}.startedAt = firebase.database.ServerValue.TIMESTAMP;
