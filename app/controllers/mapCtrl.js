553ae5489e4d9c1e6d00519824650d3e3e332f73
Danh Le <dtle82@gmail.com>
2016-12-01 17:39:23 -0800

    more zip data

--- app/controllers/mapCtrl.js
+++ app/controllers/mapCtrl.js
@@ -4,7 +4,6 @@ angular.module('realValue')
         //console.log("style", style);
         console.log("init map");
 
-
         // fixed issue when map is shown after the map container has been resized by css
         // http://stackoverflow.com/questions/24412325/resizing-a-leaflet-map-on-container-resize
         setTimeout(function(){ leafletData.getMap().then(function(map) {
@@ -12,25 +11,6 @@ angular.module('realValue')
             map.invalidateSize(false);
         });
         }, 400);
-
-        $scope.$on("leafletDirectiveMap.geojsonMouseover", function(ev, leafletEvent) {
-            countryMouseover(leafletEvent);
-            console.log(leafletEvent);
-        });
-
-        $scope.$on("leafletDirectiveMap.geojsonClick", function(ev, featureSelected, leafletEvent) {
-            console.log(leafletEvent);
-        });
-
-        function countryMouseover(leafletEvent) {
-            var layer = leafletEvent.target;
-            layer.setStyle({
-                weight: 2,
-                color: '#666',
-                fillColor: 'white'
-            });
-            //layer.bringToFront();
-        }
 
         angular.extend($scope, {
             center: {
@@ -45,7 +25,7 @@ angular.module('realValue')
                 }
             },
             geojson : {
-                data: [zip_92866,zip_92618,zip_92604,zip_92620,zip_91331],
+                data: [zip_92866,zip_92618,zip_92604,zip_92620,zip_91331,zip_92602,zip_92782,zip_93536,zip_90265,zip_92672],
                 style: style,
                 onEachFeature: function (feature, layer) {
                     // fixed issue with referencing layer inside our reset Highlight function
@@ -99,9 +79,9 @@ angular.module('realValue')
 
         function zoomToFeature(e) {
             leafletData.getMap().then(function(map) {
-                console.log(e);
-                console.log(map);
-                //map.fitBounds(e.target.getBounds());
+                //console.log(e);
+                //console.log(map);
+                map.fitBounds(e.target.getBounds());
             });
             // Modified for Angular
             // map.fitBounds(e.target.getBounds());
