41048ca99897c669a208c2e061411bee04edb098
Danh Le <dtle82@gmail.com>
2016-12-08 01:51:35 -0800

    functional jobs/crimes checkbox

--- app/controllers/navCtrl.js
+++ app/controllers/navCtrl.js
@@ -3,25 +3,27 @@ angular.module('realValue')
     .controller('navCtrl', function($mdSidenav, $mdDialog, checkboxService){
         var self = this;
         var score;
-        var crimes;
+        var attribute;
         var jobs;
+        var weight = 1;
 
-        self.types = ['jobs', 'airport', 'bar', 'cafe', 'crime', 'gas', 'gym', 'hospital', 'housing', 'library', 'museum', 'park', 'police', 'restaurant', 'school', 'traffic', 'university', 'walkScore', 'zoo'];
+        self.types = ['jobs', 'airport', 'bar', 'cafe', 'crimes', 'gas', 'gym', 'hospital', 'housing', 'library', 'museum', 'park', 'police', 'restaurant', 'school', 'traffic', 'university', 'walkScore', 'zoo'];
 
         self.updateData = function (data) {
             //checkboxService.updateSelections(data);
             self.itemPosition = self.types.indexOf(data.type);
             if (data.checked === false) {
+                this.applyUpdate(self.types[self.itemPosition], "-", 1);
                 console.log(self.types);
                 console.log(self.types.indexOf(data.type));
                 self.types.splice(self.itemPosition,1);
                 console.log(self.types);
-                this.applyUpdate("-");
+                console.log(self.itemPosition);
             } else if (data.checked === true) {
                 console.log(self.types);
                 self.types.push(data.type);
                 console.log(self.types);
-                this.deApplyUpdate("+");
+                this.applyUpdate(self.types[self.types.indexOf(data.type)], "+", 1);
             }
             self.totalChecked = self.types.length;
             console.log(self.totalChecked);
@@ -29,66 +31,41 @@ angular.module('realValue')
             county_orange.features[0].properties.score = self.totalChecked;
         };
 
-        self.applyUpdate = function(operator) {
-
-            if(self.evalCheck("crime")) {
+        self.applyUpdate = function(attr, operator, checked) {
+            if(attr === "crimes") {
+                weight = 5000;
+            }
+            console.log(attr);
+            if(checked === 1) {
                 for(var i = 0; i<tammy_geojson.features.length;i++) {
-                    if(self.hasProperty(tammy_geojson.features[i].properties,"crimes")) {
-                        //console.log(tammy_geojson.features[i] + "has property");
-                        console.log("crimes check");
-                        crimes = tammy_geojson.features[i].properties.crimes;
+                    if(self.hasProperty(tammy_geojson.features[i].properties,attr)) {
+                        console.log(tammy_geojson.features[i] + "has property");
+                        attribute = tammy_geojson.features[i].properties[attr];
                         score = tammy_geojson.features[i].properties.score;
-                        tammy_geojson.features[i].properties.score = self.doMath(parseInt(score),parseInt(jobs)*5000,operator);
+                        tammy_geojson.features[i].properties.score = self.doMath(parseInt(score),parseInt(attribute*weight),operator);
                     }
                 }
             }
 
-            if(self.evalCheck("crime")) {
+            if(checked === 1) {
                 for(var i = 0; i<tammy_geojson.features.length;i++) {
-                    if(self.hasProperty(losangeles_geojson.features[i].properties,"crimes")) {
-                        //console.log(losangeles_geojson.features[i] + "has property");
-                        console.log("crimes check");
-                        crimes = losangeles_geojson.features[i].properties.crimes;
+                    if(self.hasProperty(losangeles_geojson.features[i].properties,attr)) {
+                        console.log(losangeles_geojson.features[i] + "has property");
+                        attribute = losangeles_geojson.features[i].properties[attr];
                         score = losangeles_geojson.features[i].properties.score;
-                        losangeles_geojson.features[i].properties.score = self.doMath(parseInt(score),parseInt(crimes)*5000,operator);
-                    }
-                }
-            }
-
-            if(self.evalCheck("jobs")) {
-                for(var i = 0; i<tammy_geojson.features.length;i++) {
-                    if(self.hasProperty(tammy_geojson.features[i].properties,"jobs")) {
-                        //console.log(tammy_geojson.features[i] + "has property");
-                        console.log("jobs check");
-                        score = tammy_geojson.features[i].properties.score;
-                        jobs = tammy_geojson.features[i].properties.jobs;
-                        tammy_geojson.features[i].properties.score = self.doMath(parseInt(score),parseInt(jobs),operator);
-                    }
-                }
-            }
-
-            if(self.evalCheck("jobs")) {
-                for(var i = 0; i<losangeles_geojson.features.length;i++) {
-                    if(self.hasProperty(losangeles_geojson.features[i].properties,"jobs")) {
-                        //console.log(losangeles_geojson.features[i] + "has property");
-                        console.log("jobs check");
-                        score = losangeles_geojson.features[i].properties.score;
-                        jobs = losangeles_geojson.features[i].properties.jobs;
-                        losangeles_geojson.features[i].properties.score = self.doMath(parseInt(score),parseInt(jobs),operator);
+                        losangeles_geojson.features[i].properties.score = self.doMath(parseInt(score),parseInt(attribute*weight),operator);
                     }
                 }
             }
         };
 
-        self.evalCheck = function(attr) {
-            return (self.types.indexOf(attr) === -1);
-        }
-
         self.doMath = function(param1, param2, operator) {
             switch(operator) {
                 case "+":
+                    //console.log(param1 + param2);
                     return param1 + param2;
                 case "-":
+                    //console.log(param1 + param2);
                     return param1 - param2;
             }
         };
@@ -108,7 +85,7 @@ angular.module('realValue')
             {type: 'airport', checked: true, weight: 10},
             {type: 'bar', checked: true, weight: 10},
             {type: 'cafe', checked: true, weight: 10},
-            {type: 'crime', checked: true, weight: 10},
+            {type: 'crimes', checked: true, weight: 10},
             {type: 'gas', checked: true, weight: 10},
             {type: 'gym', checked: true, weight: 10},
             {type: 'hospital', checked: true, weight: 10},
