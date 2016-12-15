4c68f1a9c0c6879f7abcd0bad1327b173e6ba6a2
Danh Le <dtle82@gmail.com>
2016-12-15 01:26:43 -0800

    fixed api script

--- prototypes/geocoding_get.php
+++ prototypes/geocoding_get.php
@@ -1,9 +1,11 @@
 <?php
 header('Content-type: application/json');
 header("Access-Control-Allow-Origin: *");
-$address = str_replace(" ", "+",$_GET['s']);
+
 if(!isset($address)) {
     $address = 'irvine,ca';
+} else {
+    $address = str_replace(" ", "+",$_GET['s']);
 }
 $json = file_get_contents("https://maps.googleapis.com/maps/api/geocode/json?address=$address&key=AIzaSyDa6lkpC-bOxXWEbrWaPlw_FneCpQhlgNE");
 //$myXMLData = file_get_contents("http://www.zillow.com/webservice/GetRegionChildren.htm?zws-id=X1-ZWz19hii866bd7_14qhc&state=California&county=Orange&childtype=city");
