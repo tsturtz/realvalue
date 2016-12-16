67894e181747589ef9484b39fa71ab3b39891ac5
Danh Le <dtle82@gmail.com>
2016-12-16 13:02:33 -0800

    https

--- app/controllers/mapCtrl.js
+++ app/controllers/mapCtrl.js
@@ -14,7 +14,7 @@ angular.module('realValue')
             map.removeControl(map.zoomControl);
             map.options.minZoom = 9;
         });
-        }, 1500);
+        }, 500);
 
         $scope.$on("leafletDirectiveMarker.move", function(event, args){
             var leafEvent = args.leafletEvent;
@@ -285,7 +285,7 @@ angular.module('realValue')
                     zoom: param
                 }
             }
-            //console.log("center", varMap.getCenter());
+            //console.log("center", varcenter);
             angular.extend($scope, {
                 center: varcenter,
                 defaults: {
@@ -310,7 +310,7 @@ angular.module('realValue')
             tiles: {
                 //url: "http://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png",
                 //url: "http://korona.geog.uni-heidelberg.de/tiles/roadsg/x={x}&y={y}&z={z}",
-                url: "http://stamen-tiles-{s}.a.ssl.fastly.net/toner-lite/{z}/{x}/{y}.png",
+                url: "https://stamen-tiles-{s}.a.ssl.fastly.net/toner-lite/{z}/{x}/{y}.png",
                     options: {
                     //attribution: 'All maps &copy; <a href="http://www.opencyclemap.org">OpenCycleMap</a>, map data &copy; <a href="http://www.openstreetmap.org">OpenStreetMap</a> (<a href="http://www.openstreetmap.org/copyright">ODbL</a>'
                 }
