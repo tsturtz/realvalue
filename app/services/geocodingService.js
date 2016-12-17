8e6611f5ae8f266be9992d0fd224beb2a08f92b3
Danh Le <dtle82@gmail.com>
2016-12-17 13:01:55 -0800

    ssl

--- app/services/geocodingService.js
+++ app/services/geocodingService.js
@@ -15,7 +15,7 @@ angular.module('realValue')
             var deferred = $q.defer();
 
             //TODO fixed this for uploading to github/server
-            $http.get("http://35.160.245.130/c11_realvalue/prototypes/geocoding_get.php?s="+param)
+            $http.get("https://35.160.245.130/c11_realvalue/prototypes/geocoding_get.php?s="+param)
                 .then(function(response) {
                     //console.log(response);
                     deferred.resolve(response);
